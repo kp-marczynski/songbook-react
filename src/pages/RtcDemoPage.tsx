4f9965476f8ed46e8ff12126c46bc61a6cb652cb
kp-marczynski <kp.marczynski@gmail.com>
2020-08-16 12:59:43 +0200

    firestore read optimization

--- src/pages/RtcDemoPage.tsx
+++ src/pages/RtcDemoPage.tsx
@@ -16,6 +16,7 @@ import {useLocation} from "react-router";
 import SimplePeer from "simple-peer";
 import * as shortId from 'shortid';
 import {checkmark} from "ionicons/icons";
+import firebase from "firebase";
 
 const RtcDemoPage: React.FC = () => {
     const location = useLocation();
@@ -47,8 +48,9 @@ const RtcDemoPage: React.FC = () => {
         setIsIndicator(!location.pathname.includes("guest"))
 
         if (!location.pathname.includes("guest")) {
-            firestore.collection("rtc").doc(indicatorId).collection("peers").onSnapshot(snap => {
-                snap.docs.forEach(snapshot => {
+            firestore.collection("rtc").doc(indicatorId).collection("peers").where("type", "in", ["request", "answer"]).onSnapshot(collectionSnap => {
+                collectionSnap.docs.forEach(snapshot => {
+                    console.error("snapshot read")
                     if (snapshot.data().type === "request") {
                         const peer = new SimplePeer({
                             initiator: true,
@@ -56,22 +58,16 @@ const RtcDemoPage: React.FC = () => {
                         });
                         peer.on('signal', signal => {
                             if (signal.type === "offer") {
-                                firestore.collection("rtc").doc(indicatorId)
-                                    .collection("peers").doc(snapshot.id)
-                                    .set(signal).then(() => console.log("offer for id: " + snapshot.id));
+                                snapshot.ref.set(signal).then(() => console.log("offer for id: " + snapshot.id));
                             }
                         })
                         peer.on('connect', () => {
-                            firestore.collection("rtc").doc(indicatorId)
-                                .collection("peers").doc(snapshot.id)
-                                .delete()
+                            snapshot.ref.delete()
                             peer.send(JSON.stringify(stateRef.current));
                             console.log("connected with id: " + snapshot.id)
                         })
                         peer.on('error', error => {
-                            firestore.collection("rtc").doc(indicatorId)
-                                .collection("peers").doc(snapshot.id)
-                                .delete()
+                            snapshot.ref.delete()
                             peers.delete(snapshot.id)
                         })
                         peers.set(snapshot.id, peer)
@@ -82,54 +78,55 @@ const RtcDemoPage: React.FC = () => {
                         } catch (e) {
                             console.error("error for id: " + snapshot.id)
                             peers.delete(snapshot.id)
-                            firestore.collection("rtc").doc(indicatorId)
-                                .collection("peers").doc(snapshot.id)
-                                .delete()
+                            snapshot.ref.delete()
                         }
                     } else {
-                        setTimeout(() => firestore.collection("rtc").doc(indicatorId)
-                            .collection("peers").doc(snapshot.id)
-                            .delete(), 100000)
+                        setTimeout(() => snapshot.ref.delete(), 100000)
                     }
                 })
             })
         } else {
-            firestore.collection("rtc").doc(indicatorId).collection("peers").doc(peerId).onSnapshot(snapshot => {
-                const snapshotData = snapshot.data()
-                if (!snapshotData) {
+            firestore.collection("rtc").doc(indicatorId).collection("peers")
+                .where(firebase.firestore.FieldPath.documentId(), "==", peerId)
+                .where("type", "==", "offer").onSnapshot(collectionSnap => {
+                if (collectionSnap.empty) {
                     try {
                         peers.get(indicatorId).send("echo " + peerId)
                     } catch (e) {
-                        firestore.collection("rtc").doc(indicatorId).collection("peers").doc(peerId)
+                        firestore.collection("rtc").doc(indicatorId)
+                            .collection("peers").doc(peerId)
                             .set({type: "request"}).then(() => console.log("request sent"))
                     }
-                } else if (snapshotData.type === "offer") {
-                    const peer = new SimplePeer({
-                        initiator: false,
-                        trickle: false
-                    });
-                    peer.signal(snapshotData)
-                    peer.on("data", data => console.log("data: " + data))
-                    peer.on("signal", signal => {
-                        if (signal.type === "answer") {
-                            firestore.collection("rtc").doc(indicatorId)
-                                .collection("peers").doc(peerId)
-                                .set(signal).then(() => console.log("answer for id: " + snapshot.id));
-                        }
-                    })
-                    peer.on("error", error => {
-                        console.log(error)
-                    })
-                    peer.on("close", () => {
-                        console.log("connection closed")
-                        firestore.collection("rtc").doc(indicatorId).collection("peers").doc(peerId)
-                            .set({type: "request"}).then(() => console.log("request sent"))
-                    })
-                    peer.on('connect', () => {
-                        console.log("connected with id: " + peerId)
-                    })
-                    peers.set(indicatorId, peer)
                 }
+                collectionSnap.docs.forEach(snapshot => {
+                    // .doc(peerId).onSnapshot(snapshot => {
+                    console.error("snapshot read")
+                    const snapshotData = snapshot.data()
+                    if (snapshotData.type === "offer") {
+                        const peer = new SimplePeer({
+                            initiator: false,
+                            trickle: false
+                        });
+                        peer.signal(snapshotData)
+                        peer.on("data", data => console.log("data: " + data))
+                        peer.on("signal", signal => {
+                            if (signal.type === "answer") {
+                                snapshot.ref.set(signal).then(() => console.log("answer for id: " + snapshot.id));
+                            }
+                        })
+                        peer.on("error", error => {
+                            console.log(error)
+                        })
+                        peer.on("close", () => {
+                            console.log("connection closed")
+                            snapshot.ref.set({type: "request"}).then(() => console.log("request sent"))
+                        })
+                        peer.on('connect', () => {
+                            console.log("connected with id: " + peerId)
+                        })
+                        peers.set(indicatorId, peer)
+                    }
+                })
             })
         }
 
